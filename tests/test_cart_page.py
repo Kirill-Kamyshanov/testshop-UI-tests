423142b30e6d35c85fca4a468832c05accf6f572
Kirill-Kamyshanov <nestor10000@mail.ru>
2026-07-29 09:30:47 +0300

    update run_tests.yml

--- tests/test_cart_page.py
+++ tests/test_cart_page.py
@@ -1,30 +1,31 @@
 import pytest
+import allure
 
 
+@allure.feature("Cart page")
 @pytest.mark.regression
-@pytest.mark.smoke
-def test_empty_cart_displayed(cart_page):
-    """Проверка отображения элементов в пустой корзине"""
-    cart_page.open_page()
-    cart_page.check_empty_cart_page()
+class TestCartPage:
+    @pytest.mark.smoke
+    def test_empty_cart_displayed(self, cart_page):
+        """Проверка отображения элементов в пустой корзине"""
+        cart_page.open_page()
+        cart_page.check_empty_cart_page()
 
 
-@pytest.mark.regression
-@pytest.mark.smoke
-def test_cart_with_a_good(good_page, cart_page, add_test_good_in_cart):
-    """Проверка наличия товара и отображения веб-элементов в корзине с добавленным товаром"""
-    cart_page.open_page()
-    cart_page.check_enriched_cart_page()
+    @pytest.mark.smoke
+    def test_cart_with_a_good(self, good_page, cart_page, add_test_good_in_cart):
+        """Проверка наличия товара и отображения веб-элементов в корзине с добавленным товаром"""
+        cart_page.open_page()
+        cart_page.check_enriched_cart_page()
 
 
-@pytest.mark.regression
-@pytest.mark.smoke
-def test_change_goods_count_in_cart(good_page, cart_page, add_test_good_in_cart):
-    """Проверка изменения количества товара в корзине (увеличение/уменьшение/полная очистка)"""
-    cart_page.open_page()
-    cart_page.add_goods_in_cart(3)
-    cart_page.check_goods_count_in_cart(4)
-    cart_page.remove_goods_in_cart(2)
-    cart_page.check_goods_count_in_cart(2)
-    cart_page.remove_goods_in_cart()
-    cart_page.check_empty_cart_page()
+    @pytest.mark.smoke
+    def test_change_goods_count_in_cart(self, good_page, cart_page, add_test_good_in_cart):
+        """Проверка изменения количества товара в корзине (увеличение/уменьшение/полная очистка)"""
+        cart_page.open_page()
+        cart_page.add_goods_in_cart(3)
+        cart_page.check_goods_count_in_cart(4)
+        cart_page.remove_goods_in_cart(2)
+        cart_page.check_goods_count_in_cart(2)
+        cart_page.remove_goods_in_cart()
+        cart_page.check_empty_cart_page()
--- tests/test_category_page.py
+++ tests/test_category_page.py
@@ -1,29 +1,27 @@
-from time import sleep
-
 import pytest
+import allure
 
 
+@allure.feature("Category page")
 @pytest.mark.regression
-@pytest.mark.smoke
-def test_category_page_displayed(category_page):
-    """Проверка отображения страницы категории товара"""
-    category_page.open_page("/category/desks-1")
-    category_page.check_category_page_displayed()
+class TestCategoryPage:
+    @pytest.mark.smoke
+    def test_category_page_displayed(self, category_page):
+        """Проверка отображения страницы категории товара"""
+        category_page.open_page("/category/desks-1")
+        category_page.check_category_page_displayed()
 
 
-@pytest.mark.parametrize("direction", ["ASC", "DESC"])
-@pytest.mark.regression
-def test_category_page_sort(category_page, direction):
-    """Проверка сортировки товаров на странице категории по цене. Как аргумент принимает направление сортировки"""
-    category_page.open_page("/category/desks-1")
-    category_page.check_sort_by_price(direction)
-    sleep(1)
+    @pytest.mark.parametrize("direction", ["ASC", "DESC"])
+    def test_category_page_sort(self, category_page, direction):
+        """Проверка сортировки товаров на странице категории по цене. Как аргумент принимает направление сортировки"""
+        category_page.open_page("/category/desks-1")
+        category_page.check_sort_by_price(direction)
 
 
-@pytest.mark.regression
-@pytest.mark.smoke
-def test_category_page_search(category_page):
-    """Проверка функции поиска товаров на странице категории"""
-    category_page.open_page("/category/desks-1")
-    category_page.search_by_keyword("desk")
-    category_page.check_searching_results("desk")
+    @pytest.mark.smoke
+    def test_category_page_search(self, category_page):
+        """Проверка функции поиска товаров на странице категории"""
+        category_page.open_page("/category/desks-1")
+        category_page.search_by_keyword("desk")
+        category_page.check_searching_results("desk")
